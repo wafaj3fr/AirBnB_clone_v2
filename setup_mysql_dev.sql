048490de26cf46d4c7b7053ffd0b57863733e8ff
Batman99SD <azozkrg1@gmail.com>
2024-04-17 10:38:20 -0700

    edit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,7 +6,7 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
-    IF NOT EXISTS hbnb_dev@localhost
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
     IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES
    ON hbnb_dev_db.*
