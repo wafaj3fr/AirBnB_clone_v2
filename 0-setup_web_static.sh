ff324d77445307358d82192b636a822b4246ca60
wafa <wafaagaafar@gmail.com>
2024-05-06 07:06:13 -0700

    00

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -37,7 +37,7 @@ sed -i '/hbnb_static/ {
 # Restart nginx
 service nginx restart
 
-if nginx -t && service nginx reload && curl -s localhost/hbnb_static | grep -q "This is a test page for web_static deployment."; then
+if sudo nginx -t && sudo service nginx reload && curl -s localhost/hbnb_static | grep -q "This is a test page for web_static deployment."; then
     echo "Nginx configuration updated and localhost/hbnb_static available"
 else
     echo "Nginx configuration update failed or localhost/hbnb_static not available"
