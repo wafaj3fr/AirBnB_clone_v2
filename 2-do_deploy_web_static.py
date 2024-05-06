e6bc6737f89717e77236157920efcae2a12e821b
wafa <wafaagaafar@gmail.com>
2024-05-06 09:41:59 -0700

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,13 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 """
-Fabric script based on the file 1-pack_web_static.py
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import *
-import os
-
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['54.237.11.197', '35.153.16.149']
-env.user = 'ubuntu'
 
-def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
-    Returns:
-        Archive path if the archive has been correctly generated, otherwise None.
-    """
-    try:
-        # Create versions folder if it doesn't exist
-        local("mkdir -p versions")
-
-        # Generate timestamp for the archive name
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-
-        # Create the archive
-        archive_name = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(archive_name))
-
-        return archive_name
-    except Exception as e:
-        print("An error occurred:", e)
-        return None
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
