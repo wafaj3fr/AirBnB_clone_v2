02ed59bbb48df1af03a210b277d70e63596e3dbf
wafa <wafaagaafar@gmail.com>
2024-05-17 10:58:10 -0700

    5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def isnum(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def isnumt(n):
     """Returns a template"""
-    return render_template("5-number.html", n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__=="__main__":
